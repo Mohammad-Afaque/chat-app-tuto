0c7fdf39a6f5e8894443a81ff127343312c6774a
afaque-techverx <m.afaque@techverx.com>
2021-05-30 18:52:05 +0500

    Last Commit

--- src/components/Chats.js
+++ src/components/Chats.js
@@ -1,12 +1,98 @@
-import React from 'react'
+import React, { useRef, useState, useEffect } from "react"
 
-const Chats = () => {
+import axios from 'axios'
+import { useHistory } from "react-router-dom"
+import { ChatEngine } from 'react-chat-engine'
+
+import { useAuth } from "../context/authContext"
+
+import { auth } from "../firebase"
+
+export default function Chats() {
+  const didMountRef = useRef(false)
+  const [loading, setLoading] = useState(true)
+  const { user } = useAuth()
+  const history = useHistory()
+
+  async function handleLogout() {
+    await auth.signOut()
+    history.push("/")
+  }
+
+  async function getFile(url) {
+    let response = await fetch(url);
+    let data = await response.blob();
+    return new File([data], "test.jpg", { type: 'image/jpeg' });
+  }
+
+  useEffect(() => {
+    if (!didMountRef.current) {
+      didMountRef.current = true
+
+      if (!user || user === null) {
+        history.push("/")
+        return
+      }
+
+      // Get-or-Create should be in a Firebase Function
+      axios.get(
+        'https://api.chatengine.io/users/me/',
+        {
+          headers: {
+            "project-id": "ff588b7c-ece5-4644-9c32-0b309820f35c",
+            "user-name": user.email,
+            "user-secret": user.uid
+          }
+        }
+      )
+
+        .then(() => setLoading(false))
+
+        .catch(e => {
+          let formdata = new FormData()
+          formdata.append('email', user.email)
+          formdata.append('username', user.email)
+          formdata.append('secret', user.uid)
+
+          getFile(user.photoURL)
+            .then(avatar => {
+              formdata.append('avatar', avatar, avatar.name)
+
+              axios.post(
+                'https://api.chatengine.io/users/',
+                formdata,
+                { headers: { "private-key": "4ba9b18c-a0d7-47f3-9640-55224118459c" } }
+              )
+                .then(() => setLoading(false))
+                .catch(e => console.log('e', e.response))
+            })
+        })
+      // ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+
+    }
+  }, [user, history])
+
+
+  if (!user || loading) return <div />
+
   return (
-    <div>
-      <h1>
-        Hi
-    </h1>
+    <div className='chats-page'>
+      <div className='nav-bar'>
+        <div className='logo-tab'>
+          Chat App
+        </div>
+
+        <div onClick={handleLogout} className='logout-tab'>
+          Logout
+        </div>
+      </div>
+
+      <ChatEngine
+        height='calc(100vh - 66px)'
+        projectID='ff588b7c-ece5-4644-9c32-0b309820f35c'
+        userName={user.email}
+        userSecret={user.uid}
+      />
     </div>
   )
 }
-export default Chats
